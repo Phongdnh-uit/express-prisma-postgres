d6573382dde7d2a36ac3e4de933e62dfee934c16
Phongdnh-uit <dangnguyenhuyphong@gmail.com>
2025-03-27 15:41:35 +0700

    refactor task validator

--- src/validators/task.validator.ts
+++ src/validators/task.validator.ts
@@ -1,15 +1,9 @@
-import { body } from "express-validator"
+import { body } from "express-validator";
 
-const status = [
-  "PENDING",
-  "IN_PROGRESS",
-  "COMPLETED"
-]
-
+const status = ["PENDING", "IN_PROGRESS", "COMPLETED"];
 
 export const createTaskValidator = [
     body("title").notEmpty().withMessage("Title can not be empty"),
-    body("status").isIn(status).withMessage("status is not correct")
-]
-
-
+    body("status").isIn(status).withMessage("status is not correct"),
+    body("dueDate").optional().isDate().withMessage("Invalid date"),
+];
